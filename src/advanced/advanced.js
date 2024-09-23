0c2f0d59b73eaa65f2ef4445181c5dda90108494
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-09-23 10:24:20 +0200

    advanced done

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -5,22 +5,54 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 // to prove you have done this, set the variable indexOfTen to the iteration index when you find 10
 let hasTen = false
 let indexOfTen = -1
-
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true
+    indexOfTen = i
+    break
+  }
+}
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    divisibleByThreeCount++
+  }
+}
 // 3. use a for loop to find the average of the numbers in the array
 let average = 0
+let sum = 0
 
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i]
+}
+average = sum / nums.length
 // 4. use a for loop to find the largest number in the array
 let largest = 0
-
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] > largest) {
+    largest = nums[i]
+  }
+}
 // 5. use a for loop to find the smallest number in the array
 let smallest = 100000
-
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] < smallest) {
+    smallest = nums[i]
+  }
+}
 // 6. find the median of the numbers in the array
-let median = 0
 
+let median = 0
+const newNums = nums.sort((a, b) => a - b)
+const midIndex = Math.floor(newNums.length / 2)
+
+if (newNums.length % 2 === 0) {
+  median = Math.floor((newNums[midIndex - 1] + newNums[midIndex]) / 2)
+} else {
+  median = newNums[midIndex]
+}
 module.exports = {
   hasTen,
   indexOfTen,
--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -10,7 +10,9 @@ const deepThree = []
 
 // 1. Using a for loop from 1 to 10, add the value of the 'loop index' to the array 'simpleOne'
 //    eg [1,2,3...]
-
+for (let i = 1; i <= 10; i++) {
+  simpleOne.push(i)
+}
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
 // for (let i = 5; i < 7; i++) {
@@ -20,18 +22,66 @@ const deepThree = []
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
 
+for (let i = 1; i <= 10; i++) {
+  const innerArray = []
+  for (let j = 0; j < i; j++) {
+    innerArray.push(i)
+  }
+  nestedOne.push(innerArray)
+}
+
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
 
+for (let i = 1; i <= 10; i++) {
+  const innerArray = []
+  for (let j = i; j >= 1; j--) {
+    innerArray.push(j)
+  }
+  nestedTwo.push(innerArray)
+}
+
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
-
+for (let i = 1; i < 11; i++) {
+  const array = []
+  for (let j = 0; j < i; j++) {
+    const innerArray = []
+    for (let k = 0; k <= j; k++) {
+      innerArray.push(i)
+    }
+    array.push(innerArray)
+  }
+  deepOne.push(array)
+}
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
-
+for (let i = 1; i < 11; i++) {
+  const array = []
+  for (let j = 1; j <= i; j++) {
+    const innerArray = []
+    for (let k = 1; k <= j; k++) {
+      innerArray.push(k)
+    }
+    array.push(innerArray)
+  }
+  deepTwo.push(array)
+}
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
-
+for (let i = 1; i < 11; i++) {
+  const array = []
+  for (let j = 1; j <= i; j++) {
+    const innerArray = []
+    for (let k = 1; k <= j; k++) {
+      innerArray.push(k)
+    }
+    const sumOfSquares = innerArray.reduce((sum, num) => sum + num ** 2, 0)
+    const average = sumOfSquares / innerArray.length
+    array.push([average])
+  }
+  deepThree.push(array)
+}
 module.exports = {
   START,
   END,
